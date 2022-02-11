ab5aa344e1debe1cf59c3cfeaf6668ce13bac414
RoyGalaxy <ajroysstyle@gmail.com>
2022-02-11 17:37:25 +0000

    Final Version

--- src/index.py
+++ src/index.py
@@ -1,5 +1,4 @@
 # Library Management system
-# built by Roy Galaxy - 12th student and a web developer
 
 # python libraries
 import questionary as qu
@@ -8,7 +7,6 @@ import time
 # Importing local module files
 import os,sys
 sys.path.append(os.path.abspath("utils"))
-
 from books import *
 
 def getTask():
@@ -16,18 +14,8 @@ def getTask():
     index = tasks.index(task)
     actions[index]()
     print()
-    # Form self repeating loop
     time.sleep(1)
-    getTask()
-
-def performTask():
-    if(task in tasks):
-        # get user input and perform task accordingly
-        i = tasks.index(task)
-        actions[i]()
-        getTask()
-    else:
-        print("Not an available feature")
+    getTask() # Recursive Function
 
 def exitProgram():
     print("Thanks for using our program")
@@ -37,5 +25,5 @@ def exitProgram():
 tasks = ("issue","submit","add book","view books","view issued books","delete book","exit")
 actions = [issueBook,returnBook,addBook,viewBooks,viewIssued,removeBook,exitProgram]
 
-# Requesting user of task for first time
+# initiating app
 getTask()
--- src/utils/books.py
+++ src/utils/books.py
@@ -1,54 +1,24 @@
 import database as db
 
-def viewBooks():
-    print("\x1b[36mAvailable books are listed below \x1b[0m")
-    books = db.getRecords("books")
-    print("{:<6} {:<25} {:<15} {:<8}".format('Id','Title','Author','status'))
-    print("\x1b[32m",end="")
-    for book in books:
-        print("{:<6} {:<25} {:<15} {:<8}".format(book['id'],book['name'],book['author'],book['status']))
-
-def viewIssued():
-    print("All issued books")
-
-def addBook():
-    book = {
-        "name":input("Enter name of book: \x1b[32m"),
-        "author":input("\x1b[0mEnter author name: \x1b[32m"),
-        "status":"Available"
-    }
-    print("\x1b[0m")
-    db.addBook(book)
-
-def removeBook():
-    id = input("\x1b[31mEnter book id to be deleted: ")
-    db.deleteRec(f"books where bId={id}")
-    print("Book Deleted !!!\x1b[0m")
-
-def issueBook():
+def issueBook(): #Modified
     allBid = db.getColumn("bId","books")
-    
-    # get details from user
     bId = int(input("Enter book id to be issued: \x1b[32m"))
     print("\x1b[0m",end="")
-    status = db.getColumn("status",f"books where bId={bId}")[0][0]
-    if(status=="issued"):
-        print("Book already issued by someone")
-        return
-
     if((bId,) in allBid):
+        status = db.getColumn("status",f"books where bId={bId}")[0][0]
+        if(status=="issued"):
+            print("Book already issued by someone")
+            return
         name = input("Enter the name of issuer: ")
-        issuedBook = {
-            "bId":bId,
-            "issuedTo":name
-        }
-        db.issueBook(issuedBook)
+        db.addRecord("issued_books",f"{bId},'{name}'")
+        db.updateRec(f"books SET status='issued' where bId={bId}")
+        db.saveChanges(); #save changes to mysql
+        print("\x1b[36mBook Issued successfully\x1b[0m")
     else:
         print("Invalid Book Id")
 
-def returnBook():
+def returnBook(): #Modified
     allBid = db.getColumn("bId","books")
-
     bId = int(input("Enter book id to be issued: \x1b[32m"))
     print("\x1b[0m",end="")
     if((bId,) in allBid):
@@ -58,8 +28,35 @@ def returnBook():
             return
         db.deleteRec(f"issued_books where bId={bId}")
         db.updateRec(f"books set status = 'Available' where bId = {bId}")
+        db.saveChanges(); #save changes to mysql
         print("\x1b[36mBook Submitted!!!\x1b[0m")
     else:
         print("\x1b[31mBook not found in records!!!\x1b[0m")
         return
-    
+
+def addBook(): #Modified
+    name = input("Enter name of book: \x1b[32m")
+    author = input("\x1b[0mEnter author name: \x1b[32m")
+    db.addRecord("books (title,author,status)",f"'{name}','{author}','Available'")
+    print("Book Addded Successfully!!!\x1b[0m")
+    db.saveChanges(); #save changes to mysql
+
+def viewBooks(): #Modified
+    books = db.getRecords("books")
+    print("{:<6} {:<35} {:<25} {:<8}".format('Id','Title','Author','status'))
+    print("\x1b[32m",end="")
+    for book in books:
+        print("{:<6} {:<35} {:<25} {:<8}".format(book[0],book[1],book[2],book[3]))
+
+def viewIssued():
+    books = db.getRecords("issued_books ib INNER JOIN books b on ib.bId = b.bId")
+    print("{:<6} {:35} {:25} {:20}".format("Id","Title","Author","Issuer"))
+    print("\x1b[32m",end="")
+    for book in books:
+        print("{:<6} {:35} {:25} {:20}".format(book[0],book[3],book[4],book[1]))
+
+def removeBook():
+    id = input("\x1b[31mEnter book id to be deleted: ")
+    db.deleteRec(f"books where bId={id}")
+    print("Book Deleted !!!\x1b[0m")
+    db.saveChanges(); #save changes to mysql
